5d70b8bccbf9e343a07771de75d8ca1dac7097ec
christina030 <80701591+christina030@users.noreply.github.com>
2024-03-01 07:32:13 +0800

    Update diary.py

--- diary.py
+++ diary.py
@@ -9,6 +9,7 @@ import datetime
 
 from imgur import glucose_graph
 
+
 # 回覆圖片或影片訊息
 # 建立回覆圖片的函式
 def reply_img(tk, text, mood, folder, fontsize=72, color=(255,255,255), margin=20, linewidth=5, fontPath='used/CHENYULUOYAN-THIN-MONOSPACED.TTF'):
@@ -59,10 +60,10 @@ def reply_img(tk, text, mood, folder, fontsize=72, color=(255,255,255), margin=2
       return False
 
 
-def handle_diary(tk, userID, text, mood, line_bot_api):
+def handle_diary(tk, userID, text, mood, line_bot_api, folder):
     print('Text: ', text)
     if "---\n想記錄的話：\n---\n" in text:
-        img_url = reply_img(tk, text, mood)   # 取得對應的圖片，如果沒有取得，會是 False
+        img_url = reply_img(tk, text, mood, folder)   # 取得對應的圖片，如果沒有取得，會是 False
         print(img_url)
         if img_url:
             # 如果有圖片網址，回傳圖片
@@ -70,7 +71,7 @@ def handle_diary(tk, userID, text, mood, line_bot_api):
             line_bot_api.reply_message(tk, img_message)
             os.system(f'rm {img_url[1]}')
 
-            save_mood(userID, mood)
+            save_mood(userID, mood, folder)
         else:
             # 如果是 False，回傳文字
             text_message = TextSendMessage(text='找不到相關日記圖片')
@@ -78,3 +79,30 @@ def handle_diary(tk, userID, text, mood, line_bot_api):
     else:
         text_message = TextSendMessage(text='請保留\n---\n想記錄的話：\n---\n')
         line_bot_api.reply_message(tk, text_message)
+
+def save_mood(userID, mood, folder, mood_filename='mood_scores.pkl', action_filename='action_done.pkl'):
+    # check if action done
+    action_done = None
+    with open(os.path.join(folder, action_filename), 'rb') as f:
+        action_done = pickle.load(f)
+
+    today = datetime.date.today()
+    if action_done[userID] == today:
+        not_done = False
+    else:
+        action_done[userID] = today
+        with open(os.path.join(folder, action_filename), 'wb') as f:
+            pickle.dump(action_done, f)
+        not_done = True
+
+    # change mood score
+    if not_done:
+        mood_scores = None
+        with open(os.path.join(folder, mood_filename), 'rb') as f:
+            mood_scores = pickle.load(f)
+
+        mood_scores[userID][-1] += (mood + 1)
+        print(userID, '分數：', mood_scores[userID][-1])
+
+        with open(os.path.join(folder, mood_filename), 'wb') as f:
+            pickle.dump(mood_scores, f)
