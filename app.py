f707465b84b37ee49bb2af00f42e4f8a03d08e74
christina030 <80701591+christina030@users.noreply.github.com>
2024-03-01 06:11:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import os
 
+from message import check_handle_message
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
@@ -25,10 +27,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    check_handle_message(event, line_bot_api)
 
-import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
