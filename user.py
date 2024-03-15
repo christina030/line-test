81efadf3e76b99e30ddd7560637648fc29779bce
christina030 <80701591+christina030@users.noreply.github.com>
2024-03-15 23:52:28 +0800

    Update user.py

--- user.py
+++ user.py
@@ -12,8 +12,10 @@ from rep import add_row, read_data
 def check_and_save_user(userID, folder):#, user_filename='users.pkl', mood_filename='mood_scores.pkl', action_filename='action_done.pkl'):
     # user file
     users = read_data('users', 'user_id')
+    print('"""\nusers:')
     print(users)
     print(type(users))
+    print('"""')
     # users = None
     # with open(os.path.join(folder, user_filename), 'rb') as f:
     #     users = pickle.load(f)
@@ -22,27 +24,30 @@ def check_and_save_user(userID, folder):#, user_filename='users.pkl', mood_filen
     if userID in users:
         return
 
-    users[userID] = datetime.date.today()
+    # users[userID] = datetime.date.today()
 
-    with open(os.path.join(folder, user_filename), 'wb') as f:
-        pickle.dump(users, f)
+    # with open(os.path.join(folder, user_filename), 'wb') as f:
+    #     pickle.dump(users, f)
+    add_row('users', '(user_id, first_date)', (userID, datetime.date.today()))
 
     # mood score file
-    mood_scores = None
-    with open(os.path.join(folder, mood_filename), 'rb') as f:
-        mood_scores = pickle.load(f)
+    # mood_scores = None
+    # with open(os.path.join(folder, mood_filename), 'rb') as f:
+    #     mood_scores = pickle.load(f)
 
-    mood_scores[userID] = [0]
+    # mood_scores[userID] = [0]
 
-    with open(os.path.join(folder, mood_filename), 'wb') as f:
-        pickle.dump(mood_scores, f)
+    # with open(os.path.join(folder, mood_filename), 'wb') as f:
+    #     pickle.dump(mood_scores, f)
+    add_row('scores', '(user_id, score1)', (userID, 0))
 
     # action done file
-    action_done = None
-    with open(os.path.join(folder, action_filename), 'rb') as f:
-        action_done = pickle.load(f)
+    # action_done = None
+    # with open(os.path.join(folder, action_filename), 'rb') as f:
+    #     action_done = pickle.load(f)
 
-    action_done[userID] = None
+    # action_done[userID] = None
 
-    with open(os.path.join(folder, action_filename), 'wb') as f:
-        pickle.dump(action_done, f)
+    # with open(os.path.join(folder, action_filename), 'wb') as f:
+    #     pickle.dump(action_done, f)
+    add_row('actions', '(user_id)', (userID))
