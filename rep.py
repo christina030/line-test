d91f3b3e10f457a98cd94b864ddd348235ead277
christina030 <80701591+christina030@users.noreply.github.com>
2024-03-15 23:51:16 +0800

    Update rep.py

--- rep.py
+++ rep.py
@@ -6,15 +6,15 @@ import re
 DATABASE_URL = os.environ.get('DATABASE_URL')
 # # DATABASE_URL = os.popen('heroku config:get DATABASE_URL -a little-soul').read()[:-1]
 
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-# cursor = conn.cursor()
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cursor = conn.cursor()
 
-# cmd = 'DROP TABLE users;'
-# cursor.execute(cmd)
-# conn.commit()
+cmd = 'DROP TABLE actions;'
+cursor.execute(cmd)
+conn.commit()
 
-# cursor.close()
-# conn.close()
+cursor.close()
+conn.close()
 
 def table_exists(cursor, table_name):
     cmd = """SELECT table_name FROM information_schema.tables
@@ -47,7 +47,7 @@ def create_tables():
     if not table_exists(cursor, 'actions'):
         cmd = '''CREATE TABLE actions(
            user_id VARCHAR (50) NOT NULL,
-           done_date DATE NOT NULL
+           done_date DATE
         );'''
         cursor.execute(cmd)
         conn.commit()
