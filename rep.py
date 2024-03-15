6a9f3d3f03120f236b912ce5ecb3b1bb69344c25
christina030 <80701591+christina030@users.noreply.github.com>
2024-03-15 20:42:41 +0800

    Update rep.py

--- rep.py
+++ rep.py
@@ -2,17 +2,16 @@ import os
 import psycopg2
 # import dj_database_url
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
-# DATABASE_URL = os.popen('heroku config:get DATABASE_URL -a little-soul').read()[:-1]
+# DATABASE_URL = os.environ.get('DATABASE_URL')
+# # DATABASE_URL = os.popen('heroku config:get DATABASE_URL -a little-soul').read()[:-1]
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-cursor = conn.cursor()
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+# cursor = conn.cursor()
 
-cursor.close()
-conn.close()
+# cursor.close()
+# conn.close()
 
 def table_exists(cursor, table_name):
-    cursor = conn.cursor()
     sql = "SHOW TABLES LIKE '%s'" % table_name
     cursor.execute(sql)
     return cursor.fetchone() is not None
