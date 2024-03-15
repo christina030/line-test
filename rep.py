a80d8a5ee5e8bb321d24730a2b2140afedb900fb
christina030 <80701591+christina030@users.noreply.github.com>
2024-03-15 16:00:36 +0800

    Update rep.py

--- rep.py
+++ rep.py
@@ -6,17 +6,26 @@ DATABASE_URL = os.popen('heroku config:get DATABASE_URL -a little-soul').read()[
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cursor = conn.cursor()
 
-# 創造表格的SQL指令 = '''CREATE TABLE account(
-#    user_id serial PRIMARY KEY,
-#    username VARCHAR (50) UNIQUE NOT NULL,
-#    password VARCHAR (50) NOT NULL,
-#    email VARCHAR (355) UNIQUE NOT NULL,
-#    created_on TIMESTAMP NOT NULL,
-#    last_login TIMESTAMP
-# );'''
-
-# cursor.execute(創造表格的SQL指令)
-# conn.commit()
-
 cursor.close()
 conn.close()
+
+def create_tables():
+  DATABASE_URL = os.popen('heroku config:get DATABASE_URL -a little-soul').read()[:-1]
+  
+  conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+  cursor = conn.cursor()
+  
+  cmd = '''CREATE TABLE account(
+     user_id serial PRIMARY KEY,
+     username VARCHAR (50) UNIQUE NOT NULL,
+     password VARCHAR (50) NOT NULL,
+     email VARCHAR (355) UNIQUE NOT NULL,
+     created_on TIMESTAMP NOT NULL,
+     last_login TIMESTAMP
+  );'''
+  
+  cursor.execute(cmd)
+  conn.commit()
+  
+  cursor.close()
+  conn.close()
