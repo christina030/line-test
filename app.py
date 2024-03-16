d4e6267fa7935f66f8a815519680915ee1f5eb98
christina030 <80701591+christina030@users.noreply.github.com>
2024-03-16 09:02:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def callback():
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        print('""" InvalidSignatureError """')
         abort(400)
     return 'OK'
 
